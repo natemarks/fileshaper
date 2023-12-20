6a9aafd7ecb1c60cfe818d66117353b6b377b4dd
Nate Marks <nmarks@imprivata.com>
2023-12-20 09:35:09 -0500

    fix fsreport output

--- cmd/fsreport/main.go
+++ cmd/fsreport/main.go
@@ -3,24 +3,19 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 
 	"github.com/natemarks/fileshaper/internal"
 )
 
 func processArgs() (string, bool) {
 	// Define flags
+	fileFlag := flag.String("file", "", "File to process")
 	sortFlag := flag.Bool("sort", false, "Sort lines by appearance count")
 	flag.Parse()
+	fmt.Println("sortFlag:", *sortFlag)
+	fmt.Println("fileFlag:", *fileFlag)
 
-	// Get the first positional argument
-	if flag.NArg() == 0 {
-		fmt.Println("Usage: <your_program> [file_path] [-sort]")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	return flag.Arg(0), *sortFlag
+	return *fileFlag, *sortFlag
 }
 func main() {
 	filename, sort := processArgs()
@@ -31,7 +26,8 @@ func main() {
 
 	result := internal.Duplicates(lines)
 	if sort {
+		fmt.Println("Sorting by appearance count")
 		result.SortByLineNumberCount()
 	}
-	fmt.Println(result)
+	fmt.Println(result.String())
 }
